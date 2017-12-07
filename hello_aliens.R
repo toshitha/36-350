b8de61e53b530538d3485585d9983cb45b3e18a5
toshitha <toshithaj@gmail.com>
2017-12-07 18:20:25 -0500

    aliens message

---
+++ hello_aliens.R
@@ -0,0 +1,16 @@
+cat("Hello Extraterrestrials,
+My name is Toshitha and I am a human from Earth found in the Milkway Galaxy. 
+We are very excited to have found other living beings in this vast universe and would love to begin a relationship with your kind. 
+
+Just a little bit about us, we are fairly harmless creatures that are curious as can be seen by our inquisition of other worlds.
+We are carbon based creatures that survive by breathing oxygen, drinking water (H2O), and consuming food for energy. 
+
+How do your bodies function and what kind of environment is the planet that you live on?
+
+Earth is mostly made of large bodies of water, with 7 major continents (areas of land). For the most part, humans live on this land as we cannot sustain in the water for long periods of time.
+
+Thanks for reading my message and we would love to hear a reply from you guys. 
+
+Sincerely,
+Toshitha
+")
